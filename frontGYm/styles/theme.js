4aefc4785cf56fad8e1a771a5e32f621c1c4aec1
mateoggv94 <mateoggv94@fpuna.edu.py>
2022-08-09 08:38:48 -0400

    dark mod

--- frontGYm/styles/theme.js
+++ frontGYm/styles/theme.js
@@ -4,8 +4,23 @@ import { deepPurple, amber } from "@mui/material/colors";
 // Create a theme instance.
 let theme = createTheme({
   palette: {
-    primary: amber,
-    secondary: deepPurple,
+    primary: {
+      main: '#ff9800',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#303030',
+      paper: '#424242',
+    },
+    text: {
+      primary: '#fff',
+      secondary: 'rgba(255, 255, 255, 0.7)',
+      disabled: 'rgba(255, 255, 255, 0.5)',
+      hint: 'rgba(255, 255, 255, 0.5)',
+    },
+    divider: 'rgba(255, 255, 255, 0.12)',
   },
 });
 
